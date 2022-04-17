7681b57ef0a18ad42e36ad206c187f31f754a16c
bbertj <jakob.bertram@bbcag.ch>
2022-04-17 12:37:20 +0200

    [new] Frontend: Add frontend and resources

--- backend/server/api.py
+++ backend/server/api.py
@@ -1,29 +1,40 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import json
 
-from backend.util.mqtt_client import get_client
+# from backend.util.mqtt_client import get_client
 
 
 app = FastAPI()
 
-client = get_client()
-client.connect("192.168.1.113")
+origins = [
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
-def on_message(*args, **kwargs):
-    pass
+# client = get_client()
+# client.connect("192.168.1.113")
 
 
 @app.get('/')
 def index():
-    return {"test": "test"}
+    return {"status": "running"}
 
 
-@app.get('/get_coffee/')
+@app.post('/get_coffee/{room_num}')
 def get_coffee(room_num):
     dispatch_robot(room_num)
     return {"room_num": room_num, "status": True}
 
 
 def dispatch_robot(room_num):
-    client.publish("bot", payload=json.dumps({"room": room_num}))
+    # client.publish("bot", payload=json.dumps({"room": room_num}))
+    pass
---
+++ frontend/coffee_bot_frontend/src/components/CoffeePage.vue
@@ -0,0 +1,46 @@
+<template>
+  <button class="coffee-circle circle" @click="orderCoffee">Get Coffee</button>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: "CoffeePage",
+  methods: {
+    getRoom: function () {
+      return 5
+    },
+    orderCoffee: function () {
+      axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
+      axios
+          .post(`http://127.0.0.1:8000/get_coffee/${this.getRoom()}`)
+          .then((response) => {
+            console.log(response)
+          }, (error) => {
+            console.log(error)
+          })
+    }
+  }
+}
+</script>
+
+<style scoped>
+.coffee-circle {
+  width: 200px;
+  height: 200px;
+  border: #C6AF71 solid 1px;
+  color: whitesmoke;
+  font-size: 50px;
+  background-color: #ECD79E;
+}
+.coffee-circle:hover {
+  color: white;
+  background-color: #FFECB5;
+  border-color: #C6AF71;
+}
+
+.circle {
+  border-radius: 50%;
+}
+</style>
